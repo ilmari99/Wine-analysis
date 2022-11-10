3c9dc4f9fe34d60a622f3de3d619cc129319e8a2
ilmari99 <i.vahteristo@gmail.com>
2022-11-10 22:35:48 +0200

    Lisätty mallin arviointi, sekä multip_rows

--- koodi/ilmari/linregress.py
+++ koodi/ilmari/linregress.py
@@ -1,23 +1,64 @@
+from collections import Counter
 from handle import boxcox_df,max_norm
 from show import plot_model
 import pandas as pd
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 import random
 import scipy
 
+def multip_rows(df,ntimes=3,mask_cond=None):
+    if mask_cond is None:
+        mask_cond = lambda df : (np.abs(scipy.stats.zscore(df)) >= 3).any(axis=1)
+    weirds = df[mask_cond(df)]
+    print(f"Found {len(weirds)} weird rows.")
+    weirds = pd.concat([weirds.copy() for _ in range(ntimes)])
+    df = pd.concat([df,weirds])
+    return df
+
+def test_model(model,x_test,y_test,norm=True):
+    preds = model.predict(x_test)
+    preds = round(8*preds)
+    y_test = round(8*y_test)
+    print(preds)
+    print(y_test)
+    print("Neural network pedictions",Counter(preds).items())
+    print("Actual values",Counter(y_test).items())
+    count = 0
+    for obs, pred in zip(y_test,preds):
+        if obs == pred:
+            count += 1
+    print("Accuracy of model: ", round(count/len(preds),3))
+    errs = preds - y_test
+    fig,ax = plt.subplots()
+    #print(errs)
+    #ax.scatter(y_test,errs)
+    ax.hist(errs)
+    plt.show()
+
 if __name__ == "__main__":
     df = pd.read_csv("./viinidata/winequality-red.csv",sep=";")
-    #df["quality"] = df["quality"].apply(lambda x : pd.NA if ((int(x) in [5,6]) and (random.random() > 0.5)) else x)
+    # Randomly drop samples where quality is 5 or 6
     #df["quality"] = df["quality"].apply(lambda x : pd.NA if ((int(x) in [5,6]) and (random.random() > 0.1)) else x)
     df.dropna(inplace=True,axis=0)
+    #df = multip_rows(df,ntimes=3,mask_cond=lambda df : df["quality"].isin([1,2,3,7,8,9]))
+    #df = multip_rows(df,ntimes=3)
+    # Normalize e
     df = max_norm(df)   # Norm by column max
     df = df.astype(float)
     #plt.hist(df["quality"])
     #plt.show()
     # Create correlation matrix
     corr_mat = df.corr()
+    sns.heatmap(corr_mat,
+                xticklabels=corr_mat.columns.values,
+                yticklabels=corr_mat.columns.values,
+                annot=True
+                )
+    plt.show()
+    #pp = sns.pairplot(df,hue="quality")
     #plt.matshow(corr_mat)
     corr_mat.to_excel("./tulokset/corr_mat.xlsx")
     print("New correlation matrix: \n",corr_mat)
@@ -30,7 +71,7 @@ if __name__ == "__main__":
     endog = df.pop("quality")
     exog = df
     # Values to remove
-    pops = ["residual sugar","citric acid", "fixed acidity", "density","total sulfur dioxide"]
+    pops = ["residual sugar","citric acid", "fixed acidity", "density","free sulfur dioxide"]
     [exog.pop(k) for k in pops]
     print("EXOG: \n",exog)
     new_corr_mat = exog.corr()
@@ -41,8 +82,13 @@ if __name__ == "__main__":
     
     lin_model = sm.WLS(endog,exog,hasconst=True,).fit(cov_type="HAC",cov_kwds={"use_correction" : True, "maxlags":1})
     print(lin_model.summary())
-    errors = lin_model.predict(exog) - endog
+    test_model(lin_model,exog,endog)
+    exit()
+    errors = round(10*(lin_model.predict(exog) - endog))
+    plt.hist(errors)
+    plt.show()
+    print(errors)
     #errors = errors.apply(lambda x : x**2)
-    plt.scatter(endog,errors)
+    plt.scatter(10*endog,errors)
     plt.show()
     
