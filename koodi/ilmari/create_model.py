31437f748269f39b6ee3f92357d6670c72166bfe
ilmari99 <i.vahteristo@gmail.com>
2022-11-15 18:16:14 +0200

    Added some conditions to get_winedata_split

--- koodi/ilmari/create_model.py
+++ koodi/ilmari/create_model.py
@@ -9,6 +9,7 @@ import tensorflow as tf
 import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier,RandomForestRegressor
+from handle import boxcox_df
 
 from handle import max_norm
 def indices_to_one_hot(data, nb_classes):
@@ -20,14 +21,40 @@ def indices_to_one_hot(data, nb_classes):
         arr[i,int(targets[i])] = 1
     return arr
 
-def get_winedata_split(df,rm_outliers=False):
+def multip_rows(df,ntimes=3,mask_cond=None):
+    if mask_cond is None:
+        mask_cond = lambda df : (np.abs(scipy.stats.zscore(df)) >= 3).any(axis=1)
+    weirds = df[mask_cond(df)]
+    print(f"Found {len(weirds)} weird rows.")
+    weirds = pd.concat([weirds.copy() for _ in range(ntimes)])
+    df = pd.concat([df,weirds])
+    print(f"Added {len(weirds)} rows.")
+    return df
+
+def get_winedata_split(df,rm_outliers=False,boxcox=False,norm=False,add_rares = True):
     # Read and handle data
     if rm_outliers:
         df = df[(np.abs(scipy.stats.zscore(df)) < 3).all(axis=1)]
+    if boxcox:
+        cols = list(df.columns)
+        cols.remove("quality")
+        df,trans = boxcox_df(df,cols=cols)
     endog = df.pop("quality")
-    endog = pd.DataFrame(indices_to_one_hot(endog,10))
-    exog = max_norm(df)
-    return train_test_split(exog,endog,test_size=0.25,random_state=42)
+    exog = df
+    if norm:
+        exog = max_norm(df)
+    x_train,x_test,y_train,y_test = train_test_split(exog,endog,test_size=0.25,random_state=42)
+    if add_rares:
+        train = pd.concat([x_train,y_train],axis=1)
+        train = multip_rows(train,mask_cond=lambda df : df["quality"].isin([3/8 if norm else 3]),ntimes=6)
+        train = multip_rows(train,mask_cond=lambda df : df["quality"].isin([4/8 if norm else 4]),ntimes=1)
+        train = multip_rows(train,mask_cond=lambda df : df["quality"].isin([1 if norm else 8]),ntimes=5)
+        y_train = train.pop("quality")
+        x_train = train
+    
+    y_train = pd.DataFrame(indices_to_one_hot(y_train,10))
+    y_test = pd.DataFrame(indices_to_one_hot(y_test,10))
+    return x_train,x_test,y_train,y_test
 
 def test_model(model,x_test,y_test):
     preds = pd.DataFrame(model.predict(x_test))
@@ -36,18 +63,25 @@ def test_model(model,x_test,y_test):
     y_test = y_test.idxmax(axis=1)
     print(preds)
     print(y_test)
-    print("Neural network pedictions",Counter(preds).items())
-    print("Actual values",Counter(y_test).items())
+    pred_count = Counter(preds.to_numpy().flatten())
+    obs_count = Counter(y_test.to_numpy().flatten())
+    print("Neural network predictions",pred_count.items())
+    print("Actual values",obs_count.items())
     count = 0
     for obs, pred in zip(y_test,preds):
-        if obs == pred:
+        if int(obs) == int(pred):
             count += 1
     print("Accuracy of model: ", round(count/len(preds),3))
     errs = preds - y_test
     fig,ax = plt.subplots()
+    bins = list(range(9))
+    ax.bar(list(obs_count.keys()),list(obs_count.values()),label="Observations")
+    ax.bar(pred_count.keys(),pred_count.values(),label="Predictions")
     #print(errs)
     #ax.scatter(y_test,errs)
-    ax.set_title(f"Residuals")
+    #ax.set_title(f"Residuals")
+    ax.legend()
+    fig,ax = plt.subplots()
     ax.hist(errs)
     plt.show()
 
@@ -108,9 +142,9 @@ def red_wine_model(xy_splits,train=True):
     
 if __name__ == "__main__":
     df = pd.read_csv("./viinidata/winequality-red.csv",sep=";")
-    dec_tree = RandomForestRegressor(n_estimators=32,random_state=42,verbose=0,n_jobs=8)
-    xy_split = get_winedata_split(df,rm_outliers=False)
-
+    dec_tree = RandomForestRegressor(n_estimators=10,random_state=42,verbose=0,n_jobs=8)
+    xy_split = get_winedata_split(df,add_rares=True)
+    
     dec_tree.fit(np.array(xy_split[0]),np.array(xy_split[2]))
     test_model(dec_tree, np.array(xy_split[1]),np.array(xy_split[3]))
     #xy_split = get_winedata_split(df)
